17c42cb05bf8a21ade6bd612719cd739b14c5a80
Ankush Nokhwal <aknokhwal01@gmail.com>
2024-05-06 14:03:17 +0530

    Basic implementation of Sidebars and navbar

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { SearchInput } from "./serach-input";
+import { InviteButton } from "./invite-button";
+
+export const Navbar = () => {
+    return (
+        <div className="flex items-center gap-x-4 p-5">
+            <div className="hidden lg:flex lg:flex-1">
+                <SearchInput/>
+            </div>
+            <div className="block lg:hidden flex-1">
+                <OrganizationSwitcher hidePersonal appearance={
+                    {
+                        elements: {
+                            rootBox: {
+                                display: 'flex',
+                                justifyContent: "center",
+                                alignItems: "center",
+                                width: "100%",
+                                maxWidth: "376px",
+                            },
+                            organizationSwitcherTrigger: {
+                                padding: "6px",
+                                width: '100%',
+                                borderRadius: "8px",
+                                border: "1px solid #E5E7Eb",
+                                justifyContent: "space-between",
+                                backgroundColor: "white",
+                            }
+                        }
+                    }
+                }/>
+            </div>
+                <InviteButton />
+            <UserButton />
+        </div> 
+    )
+}
---
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { AlignLeft, Plus } from "lucide-react";
+import { CreateOrganization } from "@clerk/nextjs";
+import {
+    Dialog,
+    DialogContent,
+    DialogTrigger,
+    DialogClose
+} from '@/components/ui/dialog';
+import { Hint } from "@/components/hint";
+
+export const NewButton = () => {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <div className="aspect-squaer">
+                    <Hint
+                        label="Create organization"
+                        side="right"
+                        align="start"
+                        sideOffset={18}
+                    >
+                        <button className="bg-white/25 h-full w-full rounded-md flex p-1 items-center justify-center opacity-60 hover:opacity-100 transition">
+                        <Plus className="text-white" />
+                        </button>
+                    </Hint>
+                </div>
+            </DialogTrigger>
+            <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
+               <CreateOrganization routing="hash"/>
+            </DialogContent>
+        </Dialog>
+    )
+}
